ba35d110949ae98031d226b4bbf1924432eec801
ThuyDo161 <92873819+ThuyDo161@users.noreply.github.com>
2024-03-11 09:14:30 +0700

    update link img

--- model/product.php
+++ model/product.php
@@ -348,7 +348,7 @@ class Product
                 $file_chunks = explode(";base64,", $img->file);
                 $base64Img = base64_decode($file_chunks[1]);
 
-                $path = 'http://' . $_SERVER['HTTP_HOST'] . '/php/htshop/Images/product/' . $img->name;
+                $path = 'http://' . $_SERVER['HTTP_HOST'] . '/coosport-server/Images/product/' . $img->name;
                 $file = $DIR . $img->name;
                 if (file_put_contents($file, $base64Img)) {
                     $sql[] = '("' . $path . '",LAST_INSERT_ID())';
@@ -432,7 +432,7 @@ class Product
                 $file_chunks = explode(";base64,", $img->file);
                 $base64Img = base64_decode($file_chunks[1]);
 
-                $path = 'http://' . $_SERVER['HTTP_HOST'] . '/php/htshop/Images/product/' . $img->name;
+                $path = 'http://' . $_SERVER['HTTP_HOST'] . '/coosport-server/Images/product/' . $img->name;
                 $file = $DIR . $img->name;
                 if (file_put_contents($file, $base64Img)) {
                     $query2 = "UPDATE img SET img = :pathImg 
